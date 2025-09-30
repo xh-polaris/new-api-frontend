29654f01544a64fdd025d8389b8f89bc04cdb5a4
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-09-30 15:35:09 +0800

    chore: replace title and logo

--- src/components/table/channels/modals/EditChannelModal.jsx
+++ src/components/table/channels/modals/EditChannelModal.jsx
@@ -99,7 +99,7 @@ function type2secretPrompt(type) {
     case 33:
       return '按照如下格式输入：Ak|Sk|Region';
     case 50:
-      return '按照如下格式输入: AccessKey|SecretKey, 如果上游是New API，则直接输ApiKey';
+      return '按照如下格式输入: AccessKey|SecretKey, 如果上游是Inno Api，则直接输ApiKey';
     case 51:
       return '按照如下格式输入: Access Key ID|Secret Access Key';
     default:
@@ -275,7 +275,7 @@ const EditChannelModal = (props) => {
       Modal.confirm({
         title: '警告',
         content:
-          '不需要在末尾加/v1，New API会自动处理，添加后可能导致请求失败，是否继续？',
+          '不需要在末尾加/v1，Inno Api会自动处理，添加后可能导致请求失败，是否继续？',
         onOk: () => {
           setInputs((inputs) => ({ ...inputs, [name]: value }));
         },
@@ -1703,7 +1703,7 @@ const EditChannelModal = (props) => {
                         <Banner
                           type='warning'
                           description={t(
-                            '如果你对接的是上游One API或者New API等转发项目，请使用OpenAI类型，不要使用此类型，除非你知道你在做什么。',
+                            '如果你对接的是上游One API或者Inno Api等转发项目，请使用OpenAI类型，不要使用此类型，除非你知道你在做什么。',
                           )}
                           className='!rounded-lg'
                         />
